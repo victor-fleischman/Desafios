616cc31ba74f584a2cf6db95a20df3b353b3ac54
victor-fleischman <fleischmanvictor@gmail.com>
2024-04-19 20:57:03 -0300

    boceto

---
+++ desafio1/main.js
@@ -0,0 +1,7 @@
+let nombreProducto = papas;
+let precioUnitario = 500;
+let cantidadDeseada = parseInt("¿Cuantos kilos de papa quiere comprar?")
+
+let totalCompra = precioUnitario * cantidadDeseada;
+
+alert("El costo total de su compra es de: " + totalCompra)
